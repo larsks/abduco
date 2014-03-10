197aa0b9e810fbd8357e8c32190a92ae653332f7
Lars Kellogg-Stedman <lars@redhat.com>
2014-03-10 16:05:48 -0400

    teach abduco about ABDUCO_SOCKET_DIR
    one may wish to place all abduco sockets somewhere other than ~/.abduco,
    but TMPDIR may not be sufficiently granular (e.g., other sockets
    appearing in TMPDIR will confuse the output of list_session).
    
    This patch causes abduco to look for ABDUCO_SOCKET_DIR before looking at
    the value of TMPDIR.

--- abduco.c
+++ abduco.c
@@ -185,6 +185,8 @@ static int create_socket_dir() {
 	size_t maxlen = sizeof(sockaddr.sun_path);
 	char *dir = getenv("HOME");
 	if (!dir)
+		dir = getenv("ABDUCO_SOCKET_DIR");
+	if (!dir)
 		dir = getenv("TMPDIR");
 	if (!dir)
 		dir = "/tmp";
